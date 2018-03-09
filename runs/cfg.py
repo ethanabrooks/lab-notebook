e592ea620dde69b0e1185f6c2ff768c5b0b3764a
Ethan Brooks <ethanabrooks@gmail.com>
2018-03-08 19:58:46 -0500

    added expanduser

--- runs/cfg.py
+++ runs/cfg.py
@@ -8,17 +8,16 @@ class Cfg:
                  virtualenv_path=None, flags=None, quiet=False):
         if flags is None:
             flags = {}
-        self.root = Path(root)
-        self.db_path = Path(db_path)
+        self.root = Path(root).expanduser()
+        self.db_path = Path(db_path).expanduser()
         should_be_absolute = '`{}` ({}) should be absolute.'
         if not self.root.is_absolute():
             _exit(should_be_absolute.format('root', self.root))
         if not self.db_path.is_absolute():
             _exit(should_be_absolute.format('db_path', self.db_path))
         self.virtualenv_path = virtualenv_path
-        if virtualenv_path is not None and not Path(virtualenv_path).exists():
-            _exit("virtualenv path {} does not exist.".format(
-                '\n'.join(virtualenv_path)))
+        if virtualenv_path is not None and not Path(virtualenv_path).expanduser().exists():
+            _exit("virtualenv path {} does not exist.".format(virtualenv_path))
         self.dir_names = dir_names.split() if dir_names else []
         self.hidden_columns = hidden_columns.split() if hidden_columns else []
         self.flags = [k if v is None else k + '=' + v
