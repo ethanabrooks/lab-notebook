5ebb1b7621423b0baa652c886fa148fc2f6196c4
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-03 15:42:34 -0400

    changed hidden-columns to columns (columns to be shown

--- runs/commands/table.py
+++ runs/commands/table.py
@@ -5,41 +5,42 @@ from tabulate import tabulate
 
 from runs.database import DataBase
 from runs.logger import Logger
-from runs.run_entry import RunEntry
 from runs.util import comma_sep_list
 
-help = 'Only display paths matching this pattern.'
+DEFAULT_COLUMNS = ['commit', 'datetime', 'description', 'input_command']
 
 
 def add_subparser(subparsers):
+    help = 'Only display paths matching this pattern.'
     table_parser = subparsers.add_parser(
         'table', help='Display contents of run database as a table.')
     table_parser.add_argument('pattern', nargs='*', help=help, type=PurePath)
     table_parser.add_argument(
-        '--hidden-columns',
+        '--columns',
         type=comma_sep_list,
-        default='full_command,path',
-        help='Comma-separated list of columns to not display in table.')
+        default=None,
+        help='Comma-separated list of columns to display in table. Default is {}'.format(
+            ' '.join(DEFAULT_COLUMNS)))
     table_parser.add_argument(
         '--column-width',
         type=int,
         default=100,
         help='Maximum width of table columns. Longer values will '
-        'be truncated and appended with "...".')
+             'be truncated and appended with "...".')
     return table_parser
 
 
 @Logger.wrapper
 @DataBase.wrapper
-def cli(pattern: List[PurePath], db: DataBase, hidden_columns: List[str],
+def cli(pattern: List[PurePath], db: DataBase, columns: List[str],
         column_width: int, *args, **kwargs):
     db.logger.print(
-        string(*pattern, db=db, hidden_columns=hidden_columns, column_width=column_width))
+        string(*pattern, db=db, columns=columns, column_width=column_width))
 
 
-def string(*patterns, db: DataBase, hidden_columns=None, column_width: int = 100):
-    if hidden_columns is None:
-        hidden_columns = []
+def string(*patterns, db: DataBase, columns=None, column_width: int = 100):
+    if columns is None:
+        columns = DEFAULT_COLUMNS
     assert isinstance(column_width, int)
 
     def get_values(entry, key):
@@ -51,7 +52,7 @@ def string(*patterns, db: DataBase, hidden_columns=None, column_width: int = 100
         except AttributeError:
             return '_'
 
-    headers = sorted(set(RunEntry.fields()) - set(hidden_columns))
+    headers = sorted(columns)
     entries = db.descendants(*patterns) if patterns else db.all()
     db = [[e.path] + [get_values(e, key) for key in headers]
           for e in sorted(entries, key=lambda e: e.path)]
