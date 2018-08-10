c6eb8ccec133782c00965ba53c9ee8e2da49d92b
Ethan Brooks <ethanabrooks@gmail.com>
2018-08-10 13:31:23 -0400

    implemented depth in ls

--- runs/commands/ls.py
+++ runs/commands/ls.py
@@ -17,6 +17,10 @@ def add_subparser(subparsers):
         action='store_true',
         help='Print run attributes in addition to names.')
     parser.add_argument(
+        '--depth',
+        type=int,
+        help='Depth of tree to print.')
+    parser.add_argument(
         '--porcelain',
         action='store_true',
         help='Print list of path names without tree '
@@ -29,25 +33,26 @@ def add_subparser(subparsers):
 @Logger.wrapper
 @DataBase.wrapper
 def cli(patterns: List[RunPath], unless: List[RunPath], db: DataBase, porcelain: bool,
-        *args, **kwargs):
-    db.logger.print(string(*patterns, db=db, porcelain=porcelain, unless=unless))
+        depth, *args, **kwargs):
+    db.logger.print(string(*patterns, db=db, porcelain=porcelain, unless=unless,
+                           depth=depth,))
 
 
 def string(*patterns, db: DataBase, porcelain: bool = True,
-           unless: List[RunPath] = None) -> str:
+           unless: List[RunPath] = None, depth: int = None) -> str:
     return '\n'.join(
-        map(str, paths(*patterns, db=db, porcelain=porcelain, unless=unless)))
+        map(str, paths(*patterns, db=db, porcelain=porcelain, unless=unless, depth=depth)))
 
 
 def paths(*patterns, db: DataBase, porcelain: bool = True,
-          unless: List[RunPath] = None) -> List[str]:
+          unless: List[RunPath] = None, depth: int = None) -> List[str]:
     entries = db.get(
         patterns, unless=unless) if patterns else db.all(unless=unless)
     _paths = [e.path for e in entries]
-    return _paths if porcelain else tree_strings(build_tree(_paths))
+    return _paths if porcelain else tree_strings(build_tree(_paths), depth=depth)
 
 
-def build_tree(paths):
+def build_tree(paths, depth: int = None):
     aggregator = defaultdict(list)
     for path in paths:
         try:
@@ -58,21 +63,23 @@ def build_tree(paths):
             head += '/'
         aggregator[head].append(RunPath(*tail))
 
-    return {k: build_tree(v) for k, v in aggregator.items()}
+    return {k: build_tree(v, depth=depth) for k, v in aggregator.items()}
 
 
-def tree_strings(tree, prefix='', root_prefix='', root='.'):
+def tree_strings(tree, prefix='', root_prefix='', root='.', depth=None):
     yield prefix + root_prefix + root
     if root_prefix == '├── ':
         prefix += '│   '
     if root_prefix == '└── ':
         prefix += '    '
-    if tree:
+    if tree and (depth is None or depth > 0):
         items = _, *tail = tree.items()
         for (root, tree), _next in zip_longest(items, tail):
             for s in tree_strings(
                     tree=tree,
                     prefix=prefix,
                     root_prefix='├── ' if _next else '└── ',
-                    root=root):
+                    root=root,
+                    depth=None if depth is None else depth - 1
+            ):
                 yield RunPath(s)
