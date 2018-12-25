88010ecb1ee4f5e2f273cf5c6d6011e0273c1806
Ethan Brooks <ethanabrooks@gmail.com>
2018-12-25 11:53:06 -0500

    added demo_script.py

---
+++ demo/demo_script.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--write-path', type=Path, required=True)
+args = parser.parse_args()
+
+with args.write_path.open('w') as f:
+    f.write('Hello')
