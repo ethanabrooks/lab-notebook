7a7c1dfba1f812966541fdf5ce4da3d2eb9748ad
Ethan Brooks <ethanabrooks@gmail.com>
2019-07-16 08:47:07 -0400

    highlight removals in red

--- runs/transaction/removal.py
+++ runs/transaction/removal.py
@@ -3,11 +3,12 @@ from pathlib import PurePath
 
 # first party
 from runs.transaction.sub_transaction import SubTransaction
+from runs.util import RED, RESET
 
 
 class RemovalTransaction(SubTransaction):
     def validate(self):
-        self.ui.check_permission("Runs to be removed:", *self.queue)
+        self.ui.check_permission(RED, "Runs to be removed:", *self.queue, RESET)
 
     def process(self, path: PurePath):
         self.tmux(path).kill()
